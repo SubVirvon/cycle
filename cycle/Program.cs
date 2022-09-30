88c4d236b43962182db34e903ccf34f1c8db534c
Virvon <dan130205@gmail.com>
2022-09-30 09:50:43 +0300

    cycle

---
+++ cycle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputMessage;
+            int iterationsCount;
+
+            Console.WriteLine("Какое сообщение вы хотите вывести?");
+            outputMessage = Console.ReadLine();
+            Console.WriteLine("Какое количество раз вывести сообщение?");
+            iterationsCount = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < iterationsCount; i++)
+            {
+                Console.WriteLine(outputMessage);
+            }
+            Console.ReadKey();
+        }
+    }
+}
